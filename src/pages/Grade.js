2386601d3cdf7c28be1ae91363d0a7a46b8dec02
junglesub <junglesubmarine@gmail.com>
2021-04-26 15:58:29 +0900

    Display Scoresheet

--- src/pages/Grade.js
+++ src/pages/Grade.js
@@ -1,4 +1,13 @@
-import { Paper, TextField } from "@material-ui/core";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
 import React, { useEffect, useState } from "react";
 import { firebaseApp } from "../lib/firebaseApp";
@@ -6,7 +15,7 @@ import { firebaseApp } from "../lib/firebaseApp";
 const options = ["22000462"];
 
 function Grade(prop) {
-  const [gradeInfo, setGradeInfo] = useState([]);
+  const [gradeInfo, setGradeInfo] = useState({});
   const [studentInfo, setStudentInfo] = useState({});
 
   useEffect(() => {
@@ -45,10 +54,10 @@ function Grade(prop) {
   }, []);
 
   console.log(prop);
-  return gradeInfo === null ? (
+  return gradeInfo === null || !gradeInfo === {} ? (
     <h1>Error</h1>
   ) : (
-    <div>
+    <div className="Grade">
       <Autocomplete
         id="hakbun"
         options={options}
@@ -58,6 +67,39 @@ function Grade(prop) {
           <TextField {...params} label="학번" variant="outlined" />
         )}
       />
+      <div>
+        <form noValidate autoComplete="off">
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>설명</TableCell>
+                  <TableCell width="100px">최고점수</TableCell>
+                  <TableCell width="100px">점수</TableCell>
+                  <TableCell>감점사유</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {gradeInfo.points &&
+                  gradeInfo.points.map((point) => (
+                    <TableRow key={point.name}>
+                      <TableCell component="th" scope="row">
+                        {point.name}
+                      </TableCell>
+                      <TableCell align="right">{point.point}</TableCell>
+                      <TableCell align="right">
+                        <TextField />
+                      </TableCell>
+                      <TableCell align="center">
+                        <TextField fullWidth />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </form>
+      </div>
     </div>
   );
 }
