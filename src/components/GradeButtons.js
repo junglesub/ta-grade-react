2e50d7cb45f731028a1fdc77dc20277cd876d746
Jungsub Ryoo <junglesubmarine@gmail.com>
2021-06-26 16:20:22 +0900

    Remove Alert to fix lint error

--- src/components/GradeButtons.js
+++ src/components/GradeButtons.js
@@ -1,5 +1,4 @@
 import { Button } from "@material-ui/core";
-import { Alert } from "@material-ui/lab";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -7,11 +6,6 @@ function GradeButtons(prop) {
   const rootLink = `/grade/${prop.match.params.gradeID}`;
   return (
     <div>
-      {/* <Alert severity="warning">
-        부분점수 기능은 아직 미완성입니다. 버그가 있을 수 있습니다.
-        <br />
-        저장과 보기는 되지만 및 내보내기가 작동하지 않습니다.
-      </Alert> */}
       <Button component={Link} to={`${rootLink}`}>
         Grade
       </Button>
